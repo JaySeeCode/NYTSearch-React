b36b85a7dd5285bec56627489bd08c872d75f61d
Juan Carlos Gonzalez Molina <Lametsu@Jcs-MacBook-Air.T-mobile.com>
2017-07-12 16:33:02 -0400

    set up all boilerplate code, connected to mongodb, initialized db, created article model/schema. now ready to role.

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../css/Main.css';
+
+class Main extends Component{
+  render(){
+    return(
+      <div className="container">
+        <h1>Boilerplate</h1>
+        <span>Is all set up</span>
+      </div>
+    );
+  }
+}
+
+export default Main;
