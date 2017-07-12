95692390ce10b94d8b883f260b2b8bfec95822e3
Juan Carlos Gonzalez Molina <Lametsu@Jcs-MacBook-Air.T-mobile.com>
2017-07-12 17:40:57 -0400

    added some react components. started working on wireframe. no server side logic quite yet

---
+++ src/components/children/Results.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Results extends Component{
+  render(){
+    return (
+      <div className="panel panel-default" >
+        <div className="panel-heading">
+          <h3 className="panel-title text-center">Results</h3>
+        </div>
+        <div className="panel-body">
+          Search Results
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Results;
